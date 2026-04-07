703099ea006ca4956c90d1352e5b496b0fed3e0b
ngomayling <ngomayling@gmail.com>
2026-04-08 00:50:14 +0200

    add script

--- scams/models.py
+++ scams/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
+SCAM_TYPES = [
+    ("phishing", "Phishing"),
+    ("sms", "SMS Scam"),
+    ("investment", "Investment Scam"),
+    ]
+
+
 class Scam(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    scam_type = models.CharField(max_length=100)
+    scam_type = models.CharField(max_length=100,choices=SCAM_TYPES)
     date_seen = models.DateField()
     url_or_contact = models.CharField(max_length=255, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
